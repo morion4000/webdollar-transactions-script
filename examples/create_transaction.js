fb78605de68951559c06b442ec09dc80536a09cf
Ionut Moraru <morion4000@gmail.com>
2018-10-21 20:33:12 +0300

    new stuff

--- examples/create_transaction.js
+++ examples/create_transaction.js
@@ -3,24 +3,32 @@ var request = require('request');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 var api_secret = process.env.API_SECRET || '1a2a450cb29bcc2bd3a6cba8729c77c2d90d5dc6';
-var api_url = process.env.API_URL || 'http://testnet2.hoste.ro:9000';
+var api_url = process.env.API_URL || 'http://testnet5.hoste.ro:43001';
 
 var transaction_fee = process.env.FEE || 10;
 var transaction_limit = 10;
 var transactions = 0;
-var amount = process.env.AMOUNT || 11;
+var amount = process.env.AMOUNT || 20;
 
 var to_addresses = [
-  'WEBD$gA0wAI2f208tTDmzUE5cJfd+ACUB@aGszb$',
+  process.env.TO_ADDRESS_1 || 'WEBD$gA0wAI2f208tTDmzUE5cJfd+ACUB@aGszb$',
 ];
 
 console.log('sending to', to_addresses.length);
-
+/*
 var wallet = {
   "version": "0.1",
   "address": "WEBD$gCD4CFYTjWqXDiQ3s7zz@G0b#DwDjh5udb$",
   "publicKey": "f4692fa1a66995984f7c5ef1f9d08a83535986e82b786054cf92e94c5a3aea8b",
   "privateKey": "800163f94970607effb636a3cdc494d616b1c912f2b0a4594cbdc379002ffa439cf4692fa1a66995984f7c5ef1f9d08a83535986e82b786054cf92e94c5a3aea8b120dc907"
+};
+*/
+
+var wallet = process.env.WALLET ? JSON.parse(process.env.WALLET) : {
+  "version": "0.1",
+  "address": "WEBD$gDSRHzxo4hSnsxDxZ3q5maccGLgyMeH9fz$",
+  "publicKey": "daf3beeb5240a1bbae1247099ccb3b4c4d0b1b3b27c422b64e8d9aebf0d9eee1",
+  "privateKey": "809f0528e6ff08d71b8ef931630ff030dd80a65fbbde5d39c2fe854d98fe5e854edaf3beeb5240a1bbae1247099ccb3b4c4d0b1b3b27c422b64e8d9aebf0d9eee1896f1cba"
 };
 
 to_addresses.forEach(function(w, i) {
---
+++ examples/create_transaction_fan_out.js
@@ -0,0 +1,36 @@
+var request = require('request');
+
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+var api_secret = process.env.API_SECRET || '1a2a450cb29bcc2bd3a6cba8729c77c2d90d5dc6';
+var api_url = process.env.API_URL || 'http://testnet5.hoste.ro:43001';
+
+var transaction_fee = process.env.FEE || 10;
+
+var to_addresses = [{
+  address: 'WEBD$gBq40vEAH4rh2f1Imxft4G9A4H9cYf954z$',
+  amount: 20 * 10000
+}, {
+  address: 'WEBD$gA0wAI2f208tTDmzUE5cJfd+ACUB@aGszb$',
+  amount: 10 * 10000
+}];
+
+console.log('sending to', to_addresses.length);
+
+var wallet = {"version":"0.1","address":"WEBD$gDSRHzxo4hSnsxDxZ3q5maccGLgyMeH9fz$","publicKey":"daf3beeb5240a1bbae1247099ccb3b4c4d0b1b3b27c422b64e8d9aebf0d9eee1","privateKey":"809f0528e6ff08d71b8ef931630ff030dd80a65fbbde5d39c2fe854d98fe5e854edaf3beeb5240a1bbae1247099ccb3b4c4d0b1b3b27c422b64e8d9aebf0d9eee1896f1cba"};
+
+var transfer_url = api_url + '/' + api_secret + '/wallets/create-transaction/' + encodeURIComponent(wallet.address) + '/null/null/' + transaction_fee;
+
+request.post({
+  url: transfer_url,
+  body: {
+    multiple_to: to_addresses
+  },
+  json: true
+}, function(err, message, body) {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log(body);
+});
